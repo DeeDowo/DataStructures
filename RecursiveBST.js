239bc51c8b8210404c17994e5bb5d5c8785dc9a6
Dowo <jo.jo.zen501@gmail.com>
2025-12-15 23:30:00 -0600

    m

--- RecursiveBST.js
+++ RecursiveBST.js
@@ -1,49 +1,49 @@
 // Recursive BST
 
-class Node{
-    constructor(value){
-        this.value = value;
-        this.left = null;
-        this.right = null;
-    }
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 }
 
-class RecursiveBST{
-    constructor(...values){
-        this.root = null;
-        if(values.length > 0) {
-            for (const value of values) {
-                this.insert(value);
-            }
-        }
+class RecursiveBST {
+  constructor(...values) {
+    this.root = null;
+    if (values.length > 0) {
+      for (const value of values) {
+        this.insert(value);
+      }
+    }
+  }
+
+  recursiveContains(value, node = this.root) {
+    if (!node) return false;
+    if (node.value = value) return true;
+    if (value > node.value) return this.recursiveContains(value, node.right);
+    return this.recursiveContains(value, node.left);
+  }
+
+  #recursiveInsertP(value, node = this.root) {
+    if (node === null) return new Node(value);
+
+    if (node.value > value) {
+      node.left = this.#recursiveInsertP(value, node.left);
+    } else if (node.value < value) {
+      node.right = this.#recursiveInsertP(value, node.right);
     }
 
-    recursiveContains(value, node = this.root){
-        if(!node) return false;
-        if(node.value = value) return true;
-        if(value > node.value) return this.recursiveContains(value, node.right);
-        return this.recursiveContains(value, node.left);
+    return node;
+  }
+
+  recursiveInsert(value) {
+    if (!this.root) {
+      this.root = new Node(value);
+      return this;
     }
 
-    #recursiveInsertP(value, node = this.root){
-        if(node === null) return new Node(value);
-        
-        if(node.value > value){
-            node.left = this.#recursiveInsertP(value, node.left);
-        }else if(node.value < value){
-            node.right = this.#recursiveInsertP(value, node.right);
-        }
-
-        return node;
-    }
-
-    recursiveInsert(value){
-        if(!this.root) {
-            this.root = new Node(value);
-            return this;
-        }
-
-        this.#recursiveInsertP(value);
-        return this;
-    }
+    this.#recursiveInsertP(value);
+    return this;
+  }
 }
